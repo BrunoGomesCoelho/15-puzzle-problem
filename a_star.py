9914c8651fe766d2ad5053117f445f0cd6d3cdb0
BrunoGomesCoelho <brasinhacoelho@hotmail.com>
2017-10-10 20:09:56 -0300

    Start of Utils module
    (cherry picked from commit bc1d0c7)

--- a_star.py
+++ a_star.py
@@ -1,6 +1,59 @@
-from input import read_test_cases
+import time
+from queue import PriorityQueue
 
-test_cases = read_test_cases()
+from input import read_test_cases, MATRIX_SIZE
+from utils import calculate_permutations, check_answer
 
-for test_case in test_cases:
-    pass
+
+def calculate_heuristic(matrix):
+    cost = 0
+    for i in range(MATRIX_SIZE):
+        for j in range(MATRIX_SIZE):
+            num = matrix[i][j]
+            if num != i*MATRIX_SIZE + (j+1) and num != 0:
+                correct_row = (num - 1) / MATRIX_SIZE
+                correct_col = (num - 1) % MATRIX_SIZE
+                cost += abs(i - correct_row) + abs(j - correct_col)
+    return cost
+
+
+def main():
+    test_cases = read_test_cases()
+
+    for test_case in test_cases:
+        start = time.time()
+        count = -1
+        answer = "This puzzle is not solvable."
+        queue = PriorityQueue()
+        visited = set()
+        found = False
+
+        queue.put((0, test_case, ""))
+
+        while not queue.empty() and not found:
+            if count > 50:
+                break
+
+            count += 1
+            cost, matrix, current_answer = queue.get()
+
+            if check_answer(matrix):
+                answer = current_answer
+                break
+
+            permutations = calculate_permutations(matrix)
+
+            for permutation, letter in permutations:
+                if permutation not in visited:
+                    heuristic_cost = calculate_heuristic(permutation)
+                    visited.add(permutation)
+                    queue.put((heuristic_cost+cost,
+                               permutation,
+                               current_answer + letter
+                               ))
+
+        print(time.time() - start, answer)
+
+
+if __name__ == "__main__":
+    main()
