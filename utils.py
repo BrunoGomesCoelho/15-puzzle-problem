1e5220bf8bfe63e837e717d464eff9dda8ca6791
BrunoGomesCoelho <brasinhacoelho@hotmail.com>
2017-10-10 22:28:05 -0300

    output file added

--- utils.py
+++ utils.py
@@ -1,8 +1,9 @@
 from input import MATRIX_SIZE
+from copy import deepcopy
 
 
 def calculate_permutations(matrix):
-    options = [(-1, 0, "L"), (1, 0, "R"), (0, -1, "U"), (0, 1, "D")]
+    options = [(-1, 0, "U"), (1, 0, "D"), (0, -1, "L"), (0, 1, "R")]
     permutations = []
     row = col = -1
     for i in range(MATRIX_SIZE):
@@ -13,8 +14,8 @@ def calculate_permutations(matrix):
 
     for i, j, letter in options:
         if 0 <= row + i < MATRIX_SIZE and 0 <= col + j < MATRIX_SIZE:
-            temp = matrix
-            temp[row][col], temp[i][j] = temp[i][j], temp[row][col]
+            temp = deepcopy(matrix)  # creates a copy of the matrix so we dont change it
+            temp[row][col], temp[row+i][col+j] = temp[row+i][col+j], temp[row][col]
             permutations.append((temp, letter))
 
     return permutations
@@ -24,6 +25,6 @@ def check_answer(matrix):
     answer = True
     for i in range(MATRIX_SIZE):
         for j in range(1, MATRIX_SIZE + 1):
-            if matrix[i][j-1] != i*MATRIX_SIZE + j and i != MATRIX_SIZE - 1 and j != MATRIX_SIZE:
+            if matrix[i][j-1] != i*MATRIX_SIZE + j and (i != MATRIX_SIZE - 1 or j != MATRIX_SIZE):
                 answer = False
     return answer
