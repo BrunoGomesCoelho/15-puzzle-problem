0eba674545259a1821a92078c58799a2aa06af04
BrunoGomesCoelho <brasinhacoelho@hotmail.com>
2017-10-23 22:31:18 -0200

    Port to pytest

--- test.py
+++ test.py
@@ -1,20 +1,15 @@
-import unittest
 from utils import check_answer
 
 
-class TestUtils(unittest.TestCase):
+class TestInput(object):
     """
     A basic test class
     """
 
     def test_check_answer_true(self):
         ans = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]]
-        self.assertEqual(check_answer(ans), True)
+        assert check_answer(ans)
 
     def test_check_answer_false(self):
         not_ans = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 0, 15]]
-        self.assertEqual(check_answer(not_ans), False)
-
-
-if __name__ == '__main__':
-    unittest.main()
+        assert not check_answer(not_ans)
