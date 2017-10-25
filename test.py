7f4bd82b368ca0cce47d56659443e8dd6c13c9ea
BrunoGomesCoelho <brasinhacoelho@hotmail.com>
2017-10-25 14:48:00 -0200

    Add tests and readability

--- test.py
+++ test.py
@@ -1,14 +1,25 @@
 import mock
 import utils
 
-from input import read_matrix
+from input import read_matrix, read_test_cases
 
 
 class TestInput(object):
+
+    @classmethod
+    def setup_class(cls):
+        cls.array = ['1 2 3 4\n\r', '5 6 7 8\n\r', "9 10 11 12 \n\n", "13 14 15 0\n"]
+
     def test_read_matrix(self):
-        with mock.patch('builtins.input', side_effect=['1 2 3 4\n', '5 6 7 8\n', "9 10 11 12 \n", "13 14 15 0\n"]):
+        with mock.patch('builtins.input', side_effect=self.array):
             output = read_matrix()
             expected = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]]
+            assert output == expected
+
+    def test_read_test_cases(self):
+        with mock.patch('builtins.input', side_effect=["1\n"] + self.array):
+            output = read_test_cases()
+            expected = [[[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]]]
             assert output == expected
 
 
